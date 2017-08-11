20e0f904c5ace464e8649278d45f5ea22ed4a38b
jwhistlecraft <j_whistlecraft@hotmail.com>
2017-08-11 14:51:54 +0000

    commit

---
+++ Titanic/functionality.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+tP = pd.read_csv('titanic.csv')
+
+pd.set_option('display.mpl_style', 'default')
+pd.set_option('display.line_width', 5000)
+pd.set_option('display.max_columns', 60)
+
+
+
+
+
+
+
+
+
